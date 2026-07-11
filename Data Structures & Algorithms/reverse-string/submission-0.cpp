e0d80940861d8f874ffef3ffda15e2ba8a8b6655
Rohit Raj <goswamirohit2830@gmail.com>
2026-07-11 15:16:20 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        stack<char> st;
+
+        for (char x : s) {
+            st.push(x);
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            s[i] = st.top();
+            st.pop();
+        }
+    }
+};
